60a81b74ae2c7d60496fb4019533336cf574a00c
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-03 12:35:05 +0530

    added Tests for webclient

--- src/test/java/com/springboot/restclient/service/AstroServiceTest.java
+++ src/test/java/com/springboot/restclient/service/AstroServiceTest.java
@@ -3,6 +3,9 @@ package com.springboot.restclient.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +32,33 @@ class AstroServiceTest {
         assertEquals(response.number(), response.people().size());
         System.out.println(response);
     }
+    // subscribes to mono onSubscribe
+    // request for all signals in case of mono only 1 request
+    // onNext is invoked when signal came through(to get data) onNext
+    // onComplete() -> process is completed
+    @Test
+    void getAstroResponseAsync() {
+        AstroService.AstroResponse response = astroService.getAstroResponseAsync()
+                .block(Duration.ofSeconds(2));
+        assertNotNull(response);
+        assertEquals("success", response.message());
+        assertTrue(response.number() >= 0);
+        assertEquals(response.number(), response.people().size());
+        System.out.println(response);
+    }
 
+    // no block method
+    @Test
+    void getAstroResponseAsyncStepVerifier() {
+        astroService.getAstroResponseAsync() // subscribe to mono
+                .as(StepVerifier::create)
+                .assertNext(response -> {
+                    assertNotNull(response);
+                    assertEquals("success", response.message());
+                    assertTrue(response.number() >= 0);
+                    assertEquals(response.number(), response.people().size());
+                    System.out.println(response);
+                })
+                .verifyComplete(); // analogous to onComplete()
+    }
 }
